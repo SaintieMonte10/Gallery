e527ac6086d2308e52addb4f090b866fe54f3753
Clare Richards <claremonte53gmail.com>
2020-05-31 14:02:51 +0300

    updated views

--- photo/views.py
+++ photo/views.py
@@ -1,26 +1,26 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
-# from django.shortcuts import render
-# from .models import Image
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from .models import Image
 
-# def home(request):
-#     images = Image.objects.all()
-#     return render(request,'welcome.html',{"images":images})
+def home(request):
+    images = Image.objects.all()
+    return render(request,'welcome.html',{"images":images})
 
-# def search_results(request):
-#     if 'image' in request.GET and request.GET["image"]:
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
 
-#         search_term = request.GET.get("image")
-#         searched_Images = Image.search_image(search_term)
-#         message = f"{search_term}"
+        search_term = request.GET.get("image")
+        searched_Images = Image.search_image(search_term)
+        message = f"{search_term}"
 
-#         return render(request, 'search.html', {"message": message, "Images": searched_Images})
+        return render(request, 'search.html', {"message": message, "Images": searched_Images})
 
-#     else:
-#         message = "You haven't searched for any term "
-#         return render(request, 'search.html', {"message": message})
+    else:
+        message = "You haven't searched for any term "
+        return render(request, 'search.html', {"message": message})
     
-# def search_location(request,location):
-#     images_by_location = Image.filter_by_location(location)
+def search_location(request,location):
+    images_by_location = Image.filter_by_location(location)
     
-#     return render(request,'location.html', {"images":images_by_location})
+    return render(request,'location.html', {"images":images_by_location})
