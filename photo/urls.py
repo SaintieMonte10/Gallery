2ba448c1c1cdbbe9326bf8bfb2788b18a9fd3b4f
Clare Richards <claremonte53gmail.com>
2020-05-31 14:31:37 +0300

    changes images

--- photo/urls.py
+++ photo/urls.py
@@ -9,5 +9,5 @@ urlpatterns=[
     url(r'^location/(\w+)',views.search_location,name='search_location')
 ]
 
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
